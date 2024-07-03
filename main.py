5eed4ab24c529710d9f8ff54db01bec7c9261eb8
Spritualkb <3058886310@qq.com>
2024-07-03 16:00:59 +0800

    添加输出路径修改和相关使用修改优化

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ import os
 import urllib.parse
 import time
 import random
+import argparse
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from tqdm import tqdm  # 导入tqdm
+from tqdm import tqdm
+from urllib3 import Retry
 
-def save_page(book_id, sulg, path, cookies=None):
+
+def save_page(book_id, slug, path, cookies=None):
     try:
         headers = {'Cookie': cookies} if cookies else {}
         docsdata = requests.get(
-            f'https://www.yuque.com/api/docs/{sulg}?book_id={book_id}&merge_dynamic_data=false&mode=markdown',
+            f'https://www.yuque.com/api/docs/{slug}?book_id={book_id}&merge_dynamic_data=false&mode=markdown',
             headers=headers, timeout=10
         )
         if docsdata.status_code != 200:
-            print("文档下载失败 页面可能被删除 ", book_id, sulg, docsdata.content)
+            print("文档下载失败 页面可能被删除 ", book_id, slug, docsdata.content)
             return
         docsjson = json.loads(docsdata.content)
 
@@ -27,7 +29,8 @@ def save_page(book_id, sulg, path, cookies=None):
     except requests.exceptions.RequestException as e:
         print(f"请求失败: {e}")
 
-def get_book(url="https://www.yuque.com/burpheart/phpaudit", cookies=None):
+
+def get_book(url, cookies=None, output_path="download"):
     session = requests.Session()
     retries = Retry(total=5, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504])
     session.mount('https://', HTTPAdapter(max_retries=retries))
@@ -44,10 +47,11 @@ def get_book(url="https://www.yuque.com/burpheart/phpaudit", cookies=None):
     temp = {}
     md = ""
     table = str.maketrans('\/:*?"<>|\n\r', "___________")
-    if not os.path.exists(f"download/{docsjson['book']['id']}"):
-        os.makedirs(f"download/{docsjson['book']['id']}")
+    output_dir = os.path.join(output_path, str(docsjson['book']['id']))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    for doc in tqdm(docsjson['book']['toc'], desc="Downloading Documents", unit="doc"):  # 加入tqdm进度条
+    for doc in tqdm(docsjson['book']['toc'], desc="Downloading Documents", unit="doc"):
         if doc['type'] == 'TITLE' or doc['child_uuid'] != '':
             list[doc['uuid']] = {'0': doc['title'], '1': doc['parent_uuid']}
             uuid = doc['uuid']
@@ -62,8 +66,9 @@ def get_book(url="https://www.yuque.com/burpheart/phpaudit", cookies=None):
                 else:
                     temp[doc['uuid']] = list[uuid]['0'].translate(table) + '/' + temp[doc['uuid']]
                     break
-            if not os.path.exists(f"download/{docsjson['book']['id']}/{temp[doc['uuid']]}"):
-                os.makedirs(f"download/{docsjson['book']['id']}/{temp[doc['uuid']]}")
+            doc_dir = os.path.join(output_dir, temp[doc['uuid']])
+            if not os.path.exists(doc_dir):
+                os.makedirs(doc_dir)
             if temp[doc['uuid']].endswith("/"):
                 md += "## " + temp[doc['uuid']][:-1] + "\n"
             else:
@@ -77,21 +82,30 @@ def get_book(url="https://www.yuque.com/burpheart/phpaudit", cookies=None):
                     md += "  " * temp[doc['parent_uuid']].count("/") + "* [" + doc['title'] + "](" + urllib.parse.quote(
                         temp[doc['parent_uuid']] + "/" + doc['title'].translate(table) + '.md') + ")" + "\n"
                 save_page(str(docsjson['book']['id']), doc['url'],
-                          f"download/{docsjson['book']['id']}/{temp[doc['parent_uuid']]}/{doc['title'].translate(table)}.md", cookies)
+                          os.path.join(output_dir, temp[doc['parent_uuid']], doc['title'].translate(table) + '.md'),
+                          cookies)
             else:
                 md += " " + "* [" + doc['title'] + "](" + urllib.parse.quote(
                     doc['title'].translate(table) + '.md') + ")" + "\n"
                 save_page(str(docsjson['book']['id']), doc['url'],
-                          f"download/{docsjson['book']['id']}/{doc['title'].translate(table)}.md", cookies)
-            time.sleep(random.randint(1, 4))  # 每次请求后等待1到4秒的随机时间
+                          os.path.join(output_dir, doc['title'].translate(table) + '.md'), cookies)
+            time.sleep(random.randint(1, 4))
 
-    with open(f"download/{docsjson['book']['id']}/SUMMARY.md", 'w', encoding='utf-8') as f:
+    with open(os.path.join(output_dir, 'SUMMARY.md'), 'w', encoding='utf-8') as f:
         f.write(md)
 
+
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        get_book(sys.argv[1], sys.argv[2])
-    elif len(sys.argv) > 1:
-        get_book(sys.argv[1])
-    else:
-        get_book()
+    parser = argparse.ArgumentParser(description='从语雀下载书籍文档。')
+    parser.add_argument('url', nargs='?', default="https://www.yuque.com/burpheart/phpaudit", help='书籍的 URL。')
+    parser.add_argument('--cookie', default=None, help='用于认证的 Cookie。')
+    parser.add_argument('--output', default="download", help='下载文件的输出目录。')
+
+    # 如果没有提供参数，则显示帮助信息
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    get_book(args.url, args.cookie, args.output)
